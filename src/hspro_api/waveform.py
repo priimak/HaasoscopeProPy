b25c8fafa037b8ddcda89122a82e6f8181f9e0d7
Dmitri Priimak <priimak@gmail.com>
2025-05-24 14:03:28 -0500

    Added Waveform::get_t_vec(TimeUnit) func

--- src/hspro_api/waveform.py
+++ src/hspro_api/waveform.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from unlib import TimeUnit, Duration
 
 
@@ -15,6 +17,14 @@ class Waveform:
         self.trigger_pos = trigger_pos
         self.dV = dV
         self.trigger_level_V = trigger_level_V
+
+    def apply_trigger_correction(self, trigger_pos: float):
+        self.trigger_pos = int(len(self.vs) * trigger_pos)
+
+    @cache
+    def get_t_vec(self, time_unit: TimeUnit) -> list[float]:
+        dt = Duration.value_of(f"{self.dt_s} s").to_float(time_unit)
+        return [(dt * (i - self.trigger_pos)) for i in range(len(self.vs))]
 
     def __repr__(self):
         return f"Waveform({len(self.vs)})"
